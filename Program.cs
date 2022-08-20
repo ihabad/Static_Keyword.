5364a96d46908930887c192468082c86cf4228c6
ihababoudiab@gmail.com <ihababoudiab@gmail.com>
2022-08-19 17:19:46 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+using StaticExercise;
+
+TempConverter.FahrenheitToCelsius(0);
+
+var Celsuis = TempConverter.FahrenheitToCelsius(50);
+
+Console.WriteLine($"{Celsuis}");
+
+var Fahrenheit = TempConverter.CelsiusToFahrenheit(10);
+Console.WriteLine($"{Fahrenheit}");
